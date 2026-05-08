43994a41b7da39c583531098f547eb4acfdfcb64
Crystal McArdle-Ventura <162148969+CrystalMV13@users.noreply.github.com>
2026-05-08 11:12:13 -0400

    PNPM added to fish & bash

--- bash/rc.bash
+++ bash/rc.bash
@@ -46,3 +46,11 @@ unset __conda_setup
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# pnpm
+export PNPM_HOME="/home/crystal/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
