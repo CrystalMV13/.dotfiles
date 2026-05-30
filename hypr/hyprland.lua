7777ee1df4be8a6f19980f1b02e04c552e241f50
Crystal McArdle-Ventura <162148969+CrystalMV13@users.noreply.github.com>
2026-05-30 15:49:57 -0400

    set compose key for hyprland

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -234,7 +234,7 @@ hl.config({
     kb_layout  = "us",
     kb_variant = "",
     kb_model   = "",
-    kb_options = "",
+    kb_options = "compose:ins",
     kb_rules   = "",
 
     follow_mouse = 1,
