45af1ba388b598d530b07e14a7440e6512bdcb99
Crystal McArdle-Ventura <162148969+CrystalMV13@users.noreply.github.com>
2026-06-10 17:02:33 -0400

    add stuf from .profile to .bashrc

--- bash/rc.bash
+++ bash/rc.bash
@@ -21,6 +21,16 @@ alias pipin="pip install --break-system-packages"
 
 export GPG_TTY=$(tty) # to fix GPG signing with passphrase
 
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+
 eval "$(starship init bash)"
 
 # >>> conda initialize >>>
