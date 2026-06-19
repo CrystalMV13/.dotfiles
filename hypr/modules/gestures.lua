e11dbd869df1d34d06dbea7b432ccb9ab2b3dd01
Crystal McArdle-Ventura <162148969+CrystalMV13@users.noreply.github.com>
2026-06-19 16:52:56 -0400

    add overview gesture

--- hypr/modules/gestures.lua
+++ hypr/modules/gestures.lua
@@ -3,3 +3,9 @@ hl.gesture({
   direction = "horizontal",
   action = "workspace"
 })
+
+hl.gesture({
+  fingers = 3,
+  direction = "up",
+  action = function () hl.exec_cmd("qs ipc -c overview call overview toggle") end,
+})
