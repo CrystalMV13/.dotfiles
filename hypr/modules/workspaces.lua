be5558886cfb9c12ce074ffa7b1aeb748f3c2ac0
Crystal McArdle-Ventura <162148969+CrystalMV13@users.noreply.github.com>
2026-06-19 16:52:44 -0400

    terminal, thunderbird workspaces

--- hypr/modules/workspaces.lua
+++ hypr/modules/workspaces.lua
@@ -1,10 +1,32 @@
+require("common.programs")
+
+hl.config({
+  misc = {
+    focus_on_activate = true,
+  },
+})
+
+-- Terminal workspace
+hl.on("hyprland.start", function ()
+    hl.exec_cmd(terminal, { workspace = "special:terminal" })
+end)
+hl.bind("ALT + T", hl.dsp.workspace.toggle_special("terminal"))
+
+-- Thunderbird workspace
+hl.window_rule({
+  name = "thunderbird-workspace",
+  match = { class = "thunderbird" },
+  workspace = "special:thunderbird"
+})
+hl.bind("ALT + B", hl.dsp.workspace.toggle_special("thunderbird"))
+
 -- Music workspace
 hl.window_rule({
   name = "music-workspace",
   match = { title = "YouTube Music.*" },
   workspace = "special:music"
 })
-hl.bind("SUPER + M", hl.dsp.workspace.toggle_special("music"))
+hl.bind("ALT + M", hl.dsp.workspace.toggle_special("music"))
 
 -- Slack workspace
 hl.window_rule({
@@ -12,4 +34,4 @@ hl.window_rule({
   match = { class = "slack" },
   workspace = "special:slack"
 })
-hl.bind("SUPER + S", hl.dsp.workspace.toggle_special("slack"))
+hl.bind("ALT + S", hl.dsp.workspace.toggle_special("slack"))
