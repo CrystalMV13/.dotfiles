4c4773d9333e89af64904dcf482a6e2b38cf146f
Crystal McArdle-Ventura <162148969+CrystalMV13@users.noreply.github.com>
2026-06-22 13:20:17 -0400

    add automatic venving to pip on fish

--- fish/functions/pip.fish
+++ fish/functions/pip.fish
@@ -1,4 +1,14 @@
-function pip -d "Faster pip using uv, if available"
+function pip -d "Faster pip using uv, if available. Also makes a venv."
+  if not test -f .venv/bin/activate.fish
+    if command -q uv
+      uv venv
+    else
+      python -m venv .venv
+    end
+  end
+
+  source .venv/bin/activate.fish
+
   if command -q uv
     uv pip $argv
   else
