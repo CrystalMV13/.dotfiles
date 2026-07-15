fe6f5256f6f81ae06347c688933a27062bce92b3
Crystal McArdle-Ventura <162148969+CrystalMV13@users.noreply.github.com>
2026-07-14 23:44:31 -0400

    recursively require only the modules subdir, but also monitors.lua (created automatically by monique)

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -51,16 +51,5 @@ local function rrequire(folder, excluded)
   rrequire_subdirs(folder)
 end
 
--- local EXCLUDED_MODULES = {}
-
--- local modules_dir = os.getenv("HOME") .. "/.config/hypr/modules"
-
--- for filename in io.popen('ls -a "' .. modules_dir .. '"'):lines() do
---   if filename:sub(-4) == ".lua" then
---     local module_name = filename:sub(1, #filename - 4)
---     require("modules." .. module_name)
---   end
--- end
-
-
-rrequire(HYPR_DIR)
+rrequire(HYPR_DIR .. "modules/", EXCLUDED_MODULES)
+require("monitors") -- monitors.lua is the only module that lives outside modules
